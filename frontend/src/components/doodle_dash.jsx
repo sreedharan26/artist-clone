a713bd41c514f681481620242a3cebb70f29c15a
dharan <dharan262003@gmail.com>
2024-02-09 23:36:51 +0530

    Updated the data, changed some padding and margin

--- frontend/src/components/doodle_dash.jsx
+++ frontend/src/components/doodle_dash.jsx
@@ -1,12 +1,53 @@
 import '../styles/doodle.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Doodle(){
+    const [data, setDataArray] = useState(null)
+    const [index, setIndex] = useState(0);
+    const [isBig, setIsBig] = useState(false);
+
+    const fetchData = async () => {
+        try{
+            const res = await axios.get('https://artist-rituals.onrender.com/ideas')
+            // return res.data;
+            setDataArray(res.data)
+            // const index = Math.floor(Math.random() * res.data.length)
+            // setRandomIndex(index);
+            console.log(res.data)
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    useEffect(()=> {
+        const func = async () => {
+          await fetchData();
+        }
+        func();
+    }, [])
+
+    const handleClick = () => {
+        const i = data !== null && data.length > 0 ? (index+1)%data.length : 0;
+        console.log(i, data[i].idea.length)
+        if(data[i].idea.length > 40){
+            setIsBig(true)
+        }else{
+            setIsBig(false);
+        }
+        setIndex(i);
+    }
+
+
     return (
         <>
             <div className="d-container">
-                <p className="heading">Pause and Reflect</p>
-                <h1 className="doodle">Doodle Dash</h1>
-                <p className="doo-description">Set a timer for five minutes and let your hand doodle freely on a blank page. No rules, just quick, uninhibited strokes.</p>
+                <div className='heading-btn-cont'>
+                    <p className="heading">Creative Catalysts</p>
+                    <button className="d-btn" onClick={handleClick}>Shuffle</button>
+                </div>
+                <h1 className="doodle">{data!==null ? data[index].heading : 'Doodle Dash'}</h1>
+                <p className="doo-description">{data !== null ? data[index].idea : 'Set a timer for five minutes and let your hand doodle freely on a blank page. No rules, just quick, uninhibited strokes.'}</p>
             </div>
         </>
     )
--- frontend/src/components/quotes.jsx
+++ frontend/src/components/quotes.jsx
@@ -2,6 +2,7 @@ import "../styles/quotes.css"
 import image from "../assets/image3.png"
 import axios from 'axios'
 import { useEffect, useState } from "react"
+import quote from '../assets/qu.svg'
 
 export default function Quotes(){
     const [data, setDataArray] = useState(null)
@@ -10,7 +11,7 @@ export default function Quotes(){
 
     const fetchData = async () => {
         try{
-            const res = await axios.get('https://artist-rituals.onrender.com/data')
+            const res = await axios.get('https://artist-rituals.onrender.com/quotes')
             // return res.data;
             setDataArray(res.data)
             // const index = Math.floor(Math.random() * res.data.length)
@@ -50,23 +51,24 @@ export default function Quotes(){
                 <div className="left-div">
                     <div className="q-i-cont">
                         <div className="img-cont">
-                            <img className="q-image" src={image} />
+                            <img className="q-image" src={data !== null ? data[index].image[0].thumbnails.large.url : image} />
                         </div>
-                        <div className="q-i-wrapper">
+                        {/* <div className="q-i-wrapper">
 
-                        </div>
+                        </div> */}
                     </div>
                     <button className="btn" onClick={handleClick}>Shuffle</button>
                 </div>
                 <div className="right-div">
-                    <p className="q-heading">Palette Perspectives</p>
+                    <p className="q-heading">Visionary Voices</p>
                     {/* <span className="open-quote">“</span> */}
+                    <img src={quote} className="quote-image"/>
                     <h1 className={`quote ${isBig === true ? "big-quote" : '' }`}>
                         {data && data[index] ? data[index].quote  : 'Originality is undetected plagiarism'}
                         {/* {data && data[index] && data[index].quote.length > 40 ? () => setIsBig(true) : () => setIsBig(false)} */}
                     </h1>
                     <p className="name"><b>{data && data[index] ? data[index].name : 'Author'}</b></p>
-                    <p className="description">Title and what he did</p>
+                    <p className="description">{data && data[index].desc}</p>
                 </div>
             </div>
         </>
