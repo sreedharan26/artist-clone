edde016b4b15ec733191ff3848975d9a090f3b06
dharan <dharan262003@gmail.com>
2024-02-09 23:28:39 +0530

    Written the functions for fetching data for 1st row cards

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,16 @@ const app = express()
 
 app.use(cors())
 
-const base = new Airtable({apiKey: process.env.API_KEY}).base('appgxnMTUbADkra8u')
+const base = new Airtable({apiKey: process.env.API_KEY_ODB}).base('app5C3EKxBArX6f00')
 
-const table = base('Table 1');
+const table1 = base('Visionary voices');
+const table2 = base('Creative Catalyst')
 
-const getRecords = () => {
+const getQuotes = () => {
     return new Promise((resolve, reject) => {
-        table.select({
+        table1.select({
             view: 'Grid view',
-            fields: ['Name', 'Quote']
+            fields: ['author name', 'Author Headshots', 'What he did', 'Quote']
         }).firstPage((err, records) => {
             if (err) {
                 console.error(err);
@@ -22,19 +23,44 @@ const getRecords = () => {
             } else {
                 const data = records.map(record => ({
                     id: record.id,
-                    name: record.get('Name'),
+                    name: record.get('author name'),
+                    image: record.get('Author Headshots'),
+                    desc: record.get('What he did'),
                     quote: record.get('Quote')
                 }));
                 resolve(data);
             }
         });
     });
-    }
+}
+
+const getIdeas = () => {
+    return new Promise((resolve, reject) => {
+        table2.select({
+            view: 'Grid view',
+            fields: ['Heading' ,'Description']
+        }).firstPage((err, records) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                const data = records.map(record => ({
+                    id: record.id,
+                    heading: record.get('Heading'),
+                    idea: record.get('Description')
+                }));
+                resolve(data);
+            }
+        });
+    });
+}
+
+
     
-app.get('/data',  async (req, res) => {
+app.get('/quotes',  async (req, res) => {
     
     try{
-        const data =await getRecords()
+        const data =await getQuotes()
         res.json(data);
     }
     catch(e){
@@ -42,6 +68,16 @@ app.get('/data',  async (req, res) => {
     }
 })
 
+app.get('/ideas',  async (req, res) => {
+    
+    try{
+        const data =await getIdeas()
+        res.json(data);
+    }
+    catch(e){
+        res.status(500).send('Error retrieving data from Airtable');
+    }
+})
 
 
 app.listen(3000, () => console.log("Server listening on port 3000"))
