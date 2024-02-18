5bfd86db521a4d3abdd4b28c872ecc985a18597b
dharan <dharan262003@gmail.com>
2024-02-18 20:58:02 +0530

    Removed the api key

--- backend/operations/gptImages.js
+++ backend/operations/gptImages.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
+require('dotenv').config
+
+const API_KEY = process.env.OPENAPI_KEY
 
 const gptImages = async (prompt) => {
     const res = await axios.post('https://api.openai.com/v1/images/generations', 
                                     {
                                         headers: {
                                             "Content-Type": "application/json",
-                                            // Authorization: `Bearer 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`
+                                            Authorization: `Bearer ${API_KEY}`
                                         }
                                     },
                                     {
